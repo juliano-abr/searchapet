5934428bdf839fb44ac966dd67e569a8f3eb3418
juliano-abr <julianoabrs@gmail.com>
2024-02-19 13:55:49 -0300

    Latest fixes

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: '.',
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx',
+          ],
+          alias: {
+            '@components': './src/components',
+            '@pages': './src/pages',
+          },
+        },
+      ],
+    ],
+  };
 };
